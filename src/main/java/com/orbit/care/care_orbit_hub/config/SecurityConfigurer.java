869bacfbf1681ce006ae55fbce17430264f5e36f
Sairam N <sairam.nagaraj1992@gmail.com>
2024-10-01 04:56:17 +0530

    Added basic authentication

--- src/main/java/com/orbit/care/care_orbit_hub/config/SecurityConfigurer.java
+++ src/main/java/com/orbit/care/care_orbit_hub/config/SecurityConfigurer.java
@@ -1,2 +1,23 @@
-package com.orbit.care.care_orbit_hub.config;public class SecurityConfigurer {
+package com.orbit.care.care_orbit_hub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfigurer {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // spring 6 security implementation
+        http.csrf(httpSecurityCsrfConfigurer -> {
+            httpSecurityCsrfConfigurer.ignoringRequestMatchers("/**");
+        });
+
+        return http.build();
+    }
+
 }
