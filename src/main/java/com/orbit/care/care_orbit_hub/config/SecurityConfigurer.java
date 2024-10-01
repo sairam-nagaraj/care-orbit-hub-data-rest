7b17452fb614833283b7f1b9de364e0ea78e96d6
Sairam N <sairam.nagaraj1992@gmail.com>
2024-10-01 06:18:06 +0530

    Authenicate Http requests

--- src/main/java/com/orbit/care/care_orbit_hub/config/SecurityConfigurer.java
+++ src/main/java/com/orbit/care/care_orbit_hub/config/SecurityConfigurer.java
@@ -2,6 +2,7 @@ package com.orbit.care.care_orbit_hub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,12 +11,15 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfigurer {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     // spring 6 security implementation
         http.csrf(httpSecurityCsrfConfigurer -> {
             httpSecurityCsrfConfigurer.ignoringRequestMatchers("/**");
-        });
+        }).authorizeHttpRequests(authorize -> {
+            authorize.anyRequest().authenticated();
+        }).httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
